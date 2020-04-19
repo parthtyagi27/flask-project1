51f6000b8d9db96cec02ae962cd8a15976b552c8
parthtyagi27 <parthtyagi27@users.noreply.github.com>
2020-04-19 13:21:01 -0500

    Added json format to card obj and implemented representation function

--- card.py
+++ card.py
@@ -7,6 +7,7 @@ class Card(object):
         self.value = value
         self.color = color
         self.card_id = Card.card_id
+        self.json = {"id" : self.card_id, "color" : self.color, "value": self.value}
         Card.card_id = Card.card_id + 1
 
     def getValue(self):
@@ -15,5 +16,11 @@ class Card(object):
     def getColor(self):
         return self.color
 
+    def getAsDict(self):
+        return self.json
+
     def __str__(self):
+        return str(self.card_id) + " " + str(self.value) + " " + str(self.color)
+
+    def __repr__(self):
         return str(self.card_id) + " " + str(self.value) + " " + str(self.color)
